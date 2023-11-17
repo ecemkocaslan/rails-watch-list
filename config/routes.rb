fe6eb71d68808cdaca024e5bde68994d32a524fe
Ecem Kocaslan <ecemkocaslan06@hotmail.com>
2023-11-17 12:46:38 +0000

    hj

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,23 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # get 'lists', to: 'lists#index'
+  # get 'lists/:id', to: 'lists#show'
+  # get 'lists/new', to: 'lists#new'
+  # post 'lists', to: 'lists#create'
+  # get  'lists/:id/bookmarks/new', to: 'bookmarks#new'
+  # post 'lists/:id/bookmarks', to: 'bookmarks#create'
+  # delete 'bookmarks/:id', to: 'bookmarks#destroy'
+
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
+  end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
